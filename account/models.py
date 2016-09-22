bbaaac2ee7839a55a400e269d79b8585ab7abd28
johnnyliao <epyonss@gmail.com>
2016-09-22 14:03:42 +0800

    1.account model notify

--- account/models.py
+++ account/models.py
@@ -17,18 +17,10 @@ import urlparse, settings
 
 from django.utils import simplejson
 
-GENDER_CHOICES = (
-	(0, _(u'Other')),
-	(1, _(u'Male')),
-	(2, _(u'Female')),
-)
+
 
 class User(AbstractUser):
 	nickname = models.CharField(_(u"名稱"), max_length=30, default="", null=True, blank=True)
 	phone_number = models.CharField(_(u"電話號碼"), max_length=30, default="", null=True, blank=True)
 	photo = models.ImageField(upload_to='photos', max_length=255, null=True, blank=True)
-	signature = models.CharField(_(u"個人簽名"), max_length=50, default="", null=True, blank=True)
-	gender = models.IntegerField(_(u"性別"), choices=GENDER_CHOICES, default=0, null=True, blank=True, help_text='0:other, 1: male, 2: female')
-
-	ip_address = models.IPAddressField(_(u"IP Address"), null=True, blank=True)
 
